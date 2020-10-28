f2be76c4421d34f8dcf9c7974f6ef13bf7706dff
Theophile Sadio Nanzo <tsadio@terpmail.umd.edu>
2020-10-27 22:22:11 -0400

    Added goodbye

--- exercise.py
+++ exercise.py
@@ -1,3 +1,8 @@
 # Thomas Marmo, Noah Berman & Theophile Sadio Nanzo
 # INST326 UMD Fall 2020
 # Exercise: Git & GitHub
+# New Command, Theophile Sadio Nanzo
+
+
+print ("Hello Word")
+print("Goodbye")
